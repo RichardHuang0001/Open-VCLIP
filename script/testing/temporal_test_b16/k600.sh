ec5d36f0f8587a4fbc118d63b5f63277401a99e3
huangwei@hit-8x4090 <1837238363@qq.com>
2024-12-19 13:25:00 +0800

    用mena和exp分别复现了一下在k600上的测试

--- script/testing/temporal_test_b16/k600.sh
+++ script/testing/temporal_test_b16/k600.sh
@@ -1,24 +1,25 @@
-ROOT=/DDN_ROOT/ytcheng/code/Open-VCLIP
-CKPT=/DDN_ROOT/ytcheng/code/patching_checkpoint
+ROOT=/mnt/SSD8T/home/huangwei/projects/Open-VCLIP
+CKPT=/mnt/SSD8T/home/huangwei/projects/Open-VCLIP/checkpoints
 OUT_DIR=$CKPT/testing
 
-LOAD_CKPT_FILE=/DDN_ROOT/ytcheng/code/patching_checkpoint/basetraining/temporalclip_vitb16_8x16_interpolation_bugfix_0.5ratio_rand0.0_0.6sample/wa_checkpoints/swa_2_22.pth
-
 K600_split=k600_split1
+
+LOAD_CKPT_FILE=/mnt/SSD8T/home/huangwei/projects/Open-VCLIP/checkpoints/openvclip-b16/swa_2_22.pth
 PATCHING_RATIO=0.5
 
+# MODEL.TEMPORAL_MODELING_TYPE 'expand_temporal_view'  \
 cd $ROOT
 # MODEL.TEMPORAL_MODELING_TYPE 'expand_temporal_view'
 python -W ignore -u tools/run_net.py \
     --cfg configs/Kinetics/TemporalCLIP_vitb16_8x16_STAdapter.yaml \
-    --opts DATA.PATH_TO_DATA_DIR $ROOT/zs_label_db/$K600_split \
-    DATA.PATH_PREFIX /dev/shm/k600 \
+    --opts DATA.PATH_TO_DATA_DIR $ROOT/zs_label_db/$K600_split/test.csv \
+    DATA.PATH_PREFIX ../FROSTER/data/k600 \
     DATA.PATH_LABEL_SEPARATOR , \
     DATA.INDEX_LABEL_MAPPING_FILE $ROOT/zs_label_db/$K600_split/k600-openset-index2cls.json \
     TRAIN.ENABLE False \
     OUTPUT_DIR $OUT_DIR \
-    TEST.BATCH_SIZE 480 \
-    NUM_GPUS 8 \
+    TEST.BATCH_SIZE 90 \
+    NUM_GPUS 1 \
     DATA.DECODING_BACKEND "pyav" \
     MODEL.NUM_CLASSES 160 \
     TEST.CUSTOM_LOAD True \
@@ -28,6 +29,4 @@ python -W ignore -u tools/run_net.py \
     TEST.NUM_SPATIAL_CROPS 1 \
     TEST.PATCHING_MODEL True \
     TEST.PATCHING_RATIO $PATCHING_RATIO \
-    TEST.CLIP_ORI_PATH /root/.cache/clip/ViT-B-16.pt \
-
-
+    TEST.CLIP_ORI_PATH ~/.cache/clip/ViT-B-16.pt \
